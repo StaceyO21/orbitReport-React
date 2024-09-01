d944c1a17a5bf40ac1bb3c3af422ffce399e6916
StaceyO21 <staceyobrien21@gmail.com>
2024-08-31 21:41:19 -0500

    GA6

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+export default function Banner() {
+    return (
+        <div>
+         <header id="Banner">Orbit Report</header>
+        <head>
+            <link rel = "stylesheet" type = "text/css" href = "styles.css" />
+            </head>
+        <p>
+            Click on the buttons to see the satellites in that orbit type
+        </p>
+        </div>
+    );
+}
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,21 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
-  );
-};
+import satData from "./satData";
 
+const Buttons = ({ filterByType, setSat, displaySats }) => {
+      return (
+        <div className="flex-container">
+          <head>
+            <link rel = "stylesheet" type = "text/css" href = "styles.css" />
+            </head>
+          {displaySats.map((sat, id) => {
+            return (
+              <button onClick={() => filterByType(sat)} key={id}>
+                {sat} Orbit
+              </button>
+            );
+          })}
+          <button onClick={() => setSat(satData)}>All Orbits</button>
+        </div>
+      );
+    };
+  
 export default Buttons;
